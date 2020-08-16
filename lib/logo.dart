1f0bc7f89b5feddb034cc1bd3d1cdc2fd1dff80d
Mehul Pillai <mehulpillai30@gmail.com>
2020-08-16 15:13:16 -0400

    Adds loading indicator to splash screen

--- lib/logo.dart
+++ lib/logo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:rank_ten/app_theme.dart';
 
 const bRadius = Radius.circular(15.0);
@@ -12,44 +13,52 @@ class Logo extends StatelessWidget {
     final height = totalWidth / 2.7;
 
     return Center(
-      child: Container(
-        width: totalWidth,
-        height: height,
-        child: Stack(
-          children: <Widget>[
-            Positioned(
-              right: 5.0,
-              child: Container(
-                decoration: BoxDecoration(
-                    color: palePurple,
-                    borderRadius: BorderRadius.only(
-                        topRight: bRadius, bottomRight: bRadius)),
-                width: totalWidth * tenScale,
-                height: height,
-                child: Center(
-                  child: Text("10",
-                      style: Theme.of(context).primaryTextTheme.headline3),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Container(
+          width: totalWidth,
+          height: height,
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                right: 5.0,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: palePurple,
+                      borderRadius: BorderRadius.only(
+                          topRight: bRadius, bottomRight: bRadius)),
+                  width: totalWidth * tenScale,
+                  height: height,
+                  child: Center(
+                    child: Text("10",
+                        style: Theme.of(context).primaryTextTheme.headline3),
+                  ),
                 ),
               ),
-            ),
-            Positioned(
-              left: 5.0,
-              child: Container(
-                decoration: BoxDecoration(
-                    color: hanPurple, borderRadius: BorderRadius.all(bRadius)),
-                width: totalWidth * rankScale,
-                height: height,
-                child: Center(
-                    child: Text("Rank",
-                        style: Theme.of(context)
-                            .primaryTextTheme
-                            .headline3
-                            .copyWith(color: white))),
+              Positioned(
+                left: 5.0,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: hanPurple,
+                      borderRadius: BorderRadius.all(bRadius)),
+                  width: totalWidth * rankScale,
+                  height: height,
+                  child: Center(
+                      child: Text("Rank",
+                          style: Theme.of(context)
+                              .primaryTextTheme
+                              .headline3
+                              .copyWith(color: white))),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
+        SizedBox(height: 200, width: 50),
+        SpinKitCubeGrid(
+          color: hanPurple,
+          size: 70.0,
+        )
+      ]),
     );
   }
 }
