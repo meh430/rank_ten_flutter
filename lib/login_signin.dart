8ac62b9f36ab530db2de3804b85ff90fdc286d9f
Mehul Pillai <mehulpillai30@gmail.com>
2020-08-16 21:58:06 -0400

    Mostly finishes up login layout

--- lib/login_signin.dart
+++ lib/login_signin.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:rank_ten/login.dart';
 import 'package:rank_ten/logo.dart';
@@ -14,11 +15,25 @@ class _LoginSigninState extends State<LoginSignin> {
       resizeToAvoidBottomInset: false,
       body: Center(
         child: Column(
+          mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
             Logo(),
             Login(),
-            Text("Don't have an account? Sign up!")
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("Don't have an account? ",
+                    style: Theme.of(context).primaryTextTheme.headline6),
+                Text("Sign up!",
+                    style: Theme.of(context)
+                        .primaryTextTheme
+                        .headline6
+                        .copyWith(
+                            decoration: TextDecoration.underline,
+                            fontWeight: FontWeight.bold))
+              ],
+            )
           ],
         ),
       ),
