9c76c597d52906a0e9617bfbad1c6ae4a87e5518
Mehul Pillai <mehulpillai30@gmail.com>
2020-08-29 11:25:57 -0400

    Adds some more events

--- lib/events/ranked_list_events.dart
+++ lib/events/ranked_list_events.dart
@@ -8,6 +8,32 @@ class GetRankedListEvent extends RankedListEvent {
   GetRankedListEvent(this.listId);
 }
 
+class RankedListTitleEvent extends RankedListEvent {
+  final String title;
+
+  RankedListTitleEvent(this.title);
+}
+
+class RankedListPrivateEvent extends RankedListEvent {
+  final bool private;
+
+  RankedListPrivateEvent(this.private);
+}
+
+class RankedListItemUpdateEvent extends RankedListEvent {
+  final int index;
+  final String itemName, itemDescription, imageUrl;
+
+  RankedListItemUpdateEvent(
+      this.index, this.itemName, this.itemDescription, this.imageUrl);
+}
+
+class RankedListReorderEvent extends RankedListEvent {
+  final int previousPosition, newPosition;
+
+  RankedListReorderEvent(this.previousPosition, this.newPosition);
+}
+
 class CreateRankedListEvent extends RankedListEvent {
   final RankedList rankedList;
   final String token;
