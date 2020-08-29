6cabff81c02c4eaeb1268d0bd61004742a305c25
Mehul Pillai <mehulpillai30@gmail.com>
2020-08-29 10:48:25 -0400

    Adds some more actions to list repo

--- lib/components/user_preview_card.dart
+++ lib/components/user_preview_card.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:rank_ten/components/ranked_list_card_widget.dart';
+import 'package:rank_ten/misc/app_theme.dart';
 import 'package:rank_ten/models/user.dart';
+import 'package:rank_ten/providers/dark_theme_provider.dart';
 import 'package:rank_ten/routes/user_info_screen.dart';
 
 class UserPreviewCard extends StatelessWidget {
@@ -11,6 +14,7 @@ class UserPreviewCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var isDark = Provider.of<DarkThemeProvider>(context, listen: false).isDark;
     Widget profPic = Padding(
       padding: const EdgeInsets.all(10),
       child: CircleImage(
@@ -28,7 +32,7 @@ class UserPreviewCard extends StatelessWidget {
           elevation: 4,
           margin: const EdgeInsets.only(left: 10, right: 10, bottom: 12),
           shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
           child: Column(
             children: [
               Row(
@@ -56,10 +60,15 @@ class UserPreviewCard extends StatelessWidget {
               ),
               userPreview.bio.isNotEmpty
                   ? Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Text(userPreview.bio,
-                          style: Theme.of(context).textTheme.headline5),
-                    )
+                padding: const EdgeInsets.all(12.0),
+                child: Text(userPreview.bio,
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .headline5
+                        .copyWith(
+                        color: isDark ? Colors.white : secondText)),
+              )
                   : SizedBox()
             ],
           )),
--- lib/repos/ranked_list_repository.dart
+++ lib/repos/ranked_list_repository.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:rank_ten/api/rank_api.dart';
 import 'package:rank_ten/models/ranked_list.dart';
 
@@ -9,5 +10,32 @@ class RankedListRepository {
     var rankedList = RankedList.fromJson(response);
 
     return rankedList;
+  }
+
+  Future<dynamic> createRankedList(
+      {@required RankedList rankedList, @required String token}) async {
+    var response = await _api.post(
+        endpoint: '/rankedlist', data: rankedList.toJson(), bearerToken: token);
+
+    return response;
+  }
+
+  Future<dynamic> updateRankedList(
+      {@required RankedList rankedList,
+      @required String listId,
+      @required String token}) async {
+    var response = await _api.put(
+        endpoint: '/rankedlist/$listId',
+        data: rankedList.toJson(),
+        bearerToken: token);
+
+    return response;
+  }
+
+  Future<dynamic> deleteRankedList(
+      {@required String listId, @required String token}) async {
+    var response =
+        await _api.delete(endpoint: '/rankedlist/$listId', bearerToken: token);
+    return response;
   }
 }
