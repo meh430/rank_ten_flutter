f9681aa5e322ec703f9901f477c6f9f9ad5ae0fa
Mehul Pillai <mehulpillai30@gmail.com>
2020-08-29 11:15:05 -0400

    Adds list events

--- lib/events/ranked_list_events.dart
+++ lib/events/ranked_list_events.dart
@@ -1,7 +1,29 @@
+import 'package:rank_ten/models/ranked_list.dart';
+
 abstract class RankedListEvent {}
 
 class GetRankedListEvent extends RankedListEvent {
   final String listId;
 
   GetRankedListEvent(this.listId);
+}
+
+class CreateRankedListEvent extends RankedListEvent {
+  final RankedList rankedList;
+  final String token;
+
+  CreateRankedListEvent(this.rankedList, this.token);
+}
+
+class UpdateRankedListEvent extends RankedListEvent {
+  final RankedList rankedList;
+  final String token, listId;
+
+  UpdateRankedListEvent(this.rankedList, this.token, this.listId);
+}
+
+class DeleteRankedListEvent extends RankedListEvent {
+  final String token, listId;
+
+  DeleteRankedListEvent(this.token, this.listId);
 }
